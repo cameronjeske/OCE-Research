c72465a0a842076a0aa4928c269bf4fd0abbfe34
cameronjeske <99295399+cameronjeske@users.noreply.github.com>
2022-05-25 15:36:38 -0400

    changed width

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
     height: 1000,
-    width: 2000,
+    width: 4000,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
